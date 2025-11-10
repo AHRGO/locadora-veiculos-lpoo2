72d02cdd2a57bdaa541927e6feaa6ef33dbb23d6
Victor Hugo <victor.030farias@gmail.com>
2025-11-10 13:42:24 -0300

    Cria JanelaClienteView, mas não a finaliza

---
+++ src/main/java/br/com/locadora_veiculos/view/JanelaClienteView.java
@@ -0,0 +1,35 @@
+package br.com.locadora_veiculos.view;
+
+import br.com.locadora_veiculos.controller.ClienteController;
+
+public class JanelaClienteView extends javax.swing.JFrame {
+    private BotoesClienteView botoesClienteView;
+//    private FormularioClienteView formularioClienteView;
+    private TabelaClienteView tabelaClienteView;
+
+    public JanelaClienteView() {
+//        initComponents();
+    }
+
+    public void setController(
+            //TODO: CRIAR CLIENTE CONTROLLER
+        ClienteController controller
+    ) {
+        //TODO: CRIAR BOTOES CLIENTE VIEW
+//        botoesClienteView.setController(controller);
+    }
+
+    public void initView() {
+//        tabelaClienteView.setJanelaView(this);
+        java.awt.EventQueue.invokeLater(() -> this.setVisible(true));
+    }
+
+
+
+
+
+    //TODO: CRIAR BOTOES CLIENTE VIEW
+    //TODO: CRIAR FORMULARIO CLIENTE VIEW
+
+
+}
