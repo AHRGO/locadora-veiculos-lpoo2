6d06c41090ed7389b5a732cb14a53e8acec5026a
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-11-11 16:33:11 +0000

    Finaliza TabelaClienteView (no test)

--- src/main/java/br/com/locadora_veiculos/view/TabelaClienteView.java
+++ src/main/java/br/com/locadora_veiculos/view/TabelaClienteView.java
@@ -2,11 +2,83 @@ package br.com.locadora_veiculos.view;
 
 import javax.swing.*;
 
-public class TabelaClienteView extends javax.swing.JPanel{
+public class TabelaClienteView extends javax.swing.JPanel {
     private ClienteTableModel clienteTableModel = new ClienteTableModel();
+    private JanelaClienteView JanelaClienteView janela;
+    private int linhaClicadaParaAtualizacao = -1;
 
-    private void initPanel() {
-        JScrollPane scrollPane = new JScrollPane();
-        JTable tableView = new javax.swing.JTable();
+    private JScrollPane scrollPane //javax.swing.JScrollPane
+    private JTable tabelaCliente //javax.swing.JTable
+
+    public TabelaClienteView() {
+        initComponents();
     }
+
+    private void initComponents() {
+        scrollPane = new JScrollPane();
+        tabelaCliente = new JTable();
+
+        tabelaCliente.setModel(clienteTableModel);
+        tabelaCliente.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent event) {
+                tabelaClienteMouseClicked(event);
+            }
+        });
+
+        scrollPane.setViewportView(tabelaCliente);
+
+        GroupLayout layout = new GroupLayout(this);
+        this.setLayout(layout);
+
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+        );
+
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+        )
+    }
+
+    private void tabelaClienteMouseClicked(MouseEvent event) {
+        linhaClicadaParaAtualizacao = this.tabelaCliente.rowAtPoint(event.getPoint());
+
+        Cliente cliente = clienteTableModel.getCliente(linhaClicadaParaAtualizacao);
+
+        janela.getFormularioClienteView().setContato(contato);
+    }
+
+    public JTable getTabelaCliente() {
+        return tabelaCliente;
+    }
+
+
+    public void setJanelaView(JanelaClienteView janela) {
+        this.janela = janela;
+    }
+
+    public void setListaClientesTabela(List<Cliente> clientes) {
+        clienteTableModel.setListaClientes(clientes);
+    }
+
+    public List<Cliente> getClientesParaExcluirDaTabela() {
+        int[] linhasSelecionadas = this.getTabelaCliente().getSelectedRows();
+        List<Cliente> clientesParaExcluir = new ArrayList<>();
+
+        //TODO: VALIDAR UMA FORMA MAIS EFICIENTE DE FAZER ISSO AQUI
+        for (int i = 0; i < linhasSelecionadas.length; i++) {
+            Cliente cliente = clienteTableModel.getCliente(linhasSelecionadas[i]);
+            clientesParaExcluir.add(cliente);
+        }
+    }
+
+    public void excluirClientesDaTabela(List<Cliente> clientesParaExcluir) {
+        clienteTableModel.removeMultiplosClientes(clientesParaExcluir);
+    }
+
+    public void atualizarClienteNaTabela(Cliente cliente) {
+        clienteTableModel.fireTableRowsUpdated(linhaClicadaParaAtualizacao, linhaClicadaParaAtualizacao)
+    }
+
 }
