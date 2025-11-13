218d19aedba00aec506dd46803270e128364640c
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-11-13 20:12:55 +0000

    Altera o model de motocicleta, preparando pro dao

--- src/main/java/br/com/locadora_veiculos/model/veiculo/Motocicleta.java
+++ src/main/java/br/com/locadora_veiculos/model/veiculo/Motocicleta.java
@@ -6,16 +6,21 @@ import br.com.locadora_veiculos.model.veiculo.enums.Marca;
 import br.com.locadora_veiculos.model.veiculo.enums.ModeloMotocicleta;
 
 public class Motocicleta extends Veiculo {
+    private Long id;
     private ModeloMotocicleta modelo;
+
+    public Motocicleta(Locacao locacao, Marca marca, Estado estado, Categoria categoria, double valorDaCompra, String placa, int ano, Long veiculoId, ModeloMotocicleta modelo) {
+        super(locacao, marca, estado, categoria, valorDaCompra, placa, ano);
+        this.id = veiculoId;
+        this.modelo = modelo;
+    }
 
     public Motocicleta(Locacao locacao, Marca marca, Estado estado, Categoria categoria, double valorDaCompra, String placa, int ano, ModeloMotocicleta modelo) {
         super(locacao, marca, estado, categoria, valorDaCompra, placa, ano);
         this.modelo = modelo;
     }
 
-    public ModeloMotocicleta getModelo() {
-        return this.modelo;
-    }
+
 
     @Override
     public double getValorDiariaLocacao() {
@@ -25,4 +30,47 @@ public class Motocicleta extends Veiculo {
             case LUXO -> 300.00;
         };
     }
+
+
+
+    public ModeloMotocicleta getModelo() {
+        return this.modelo;
+    }
+
+
+    public Long getVeiculoId() {
+        return this.id;
+    }
+    public void setVeiculoId(Long veiculoId) {
+        this.id = veiculoId;
+    }
+
+
+
+    public static Motocicleta convertFromDao(ResultSet result) throws SQLException {
+        Long id = result.getLong("id_veiculo");
+        Marca marca = Marca.fromString(result.getString("marca"));
+        Estado estado = Estado.fromString(result.getString("estado"));
+        Categoria categoria = Categoria.fromString(result.getString("categoria"));
+        ModeloMotocicleta modelo = ModeloMotocicleta.fromString(result.getString("modelo"));
+        double valorDaCompra = result.getDouble("valor_compra");
+        String placa = result.getString("placa");
+        int ano = result.getInt("ano");
+
+        Locacao locacao = new Locacao();
+
+        return new Motocicleta(locacao, marca, estado, categoria, valorDaCompra, placa, ano, id, modelo);
+    }
+
+
+    public static List<Motocicleta> converListFromDao(ResultSet result) throws SQLException {
+        List<Motocicleta> motos = new ArrayList<>();
+
+        while (result.next()) {
+            motos.add(Motocicleta.convertFromDao(result));
+        }
+
+        return motos;
+    }
+    
 }
