3a6f57b0e42de92560e6391401f9af2ca3adc23e
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-11-13 20:24:04 +0000

    Atualiza model de Van para funcionar com o dao sql

--- src/main/java/br/com/locadora_veiculos/model/veiculo/Van.java
+++ src/main/java/br/com/locadora_veiculos/model/veiculo/Van.java
@@ -6,16 +6,20 @@ import br.com.locadora_veiculos.model.veiculo.enums.Marca;
 import br.com.locadora_veiculos.model.veiculo.enums.ModeloVan;
 
 public class Van extends Veiculo {
+    private Long id;
     private ModeloVan modelo;
+
+    public Van(Locacao locacao, Marca marca, Estado estado, Categoria categoria, double valorDaCompra, String placa, int ano, Long veiculoId, ModeloAutomovel modelo) {
+        super(locacao, marca, estado, categoria, valorDaCompra, placa, ano);
+        this.id = veiculoId;
+        this.modelo = modelo;
+    }
 
     public Van(Locacao locacao, Marca marca, Estado estado, Categoria categoria, double valorDaCompra, String placa, int ano, ModeloVan modelo) {
         super(locacao, marca, estado, categoria, valorDaCompra, placa, ano);
         this.modelo = modelo;
     }
 
-    public ModeloVan getModelo() {
-        return this.modelo;
-    }
 
     @Override
     public double getValorDiariaLocacao() {
@@ -25,4 +29,46 @@ public class Van extends Veiculo {
             case LUXO -> 600.00;
         };
     }
+
+
+
+    public ModeloVan getModelo() {
+        return this.modelo;
+    }
+
+
+    public Long getVeiculoId() {
+        return this.id;
+    }
+    public void setVeiculoId(Long veiculoId) {
+        this.id = veiculoId;
+    }
+
+
+    public static Van convertFromDao(ResultSet result) throws SQLException {
+        Long id = result.getLong("id_veiculo");
+        Marca marca = Marca.fromString(result.getString("marca"));
+        Estado estado = Estado.fromString(result.getString("estado"));
+        Categoria categoria = Categoria.fromString(result.getString("categoria"));
+        ModeloVan modelo = ModeloVan.fromString(result.getString("modelo"));
+        double valorDaCompra = result.getDouble("valor_compra");
+        String placa = result.getString("placa");
+        int ano = result.getInt("ano");
+
+        Locacao locacao = new Locacao();
+
+        return new Van(locacao, marca, estado, categoria, valorDaCompra, placa, ano, id, modelo);
+    }
+
+
+    public static List<Van> converListFromDao(ResultSet result) throws SQLException {
+        List<Van> vans = new ArrayList<>();
+
+        while (result.next()) {
+            vans.add(Van.convertFromDao(result));
+        }
+
+        return vans;
+    }
+    
 }
