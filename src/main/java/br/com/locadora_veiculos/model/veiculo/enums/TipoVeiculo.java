1e0b7410a192035e144426d84d3794b2578ec18c
Victor Farias <60015667+AHRGO@users.noreply.github.com>
2025-11-13 19:27:58 +0000

    Cria enum do tipo do veiculo, para salvar no banco

---
+++ src/main/java/br/com/locadora_veiculos/model/veiculo/enums/TipoVeiculo.java
@@ -0,0 +1,30 @@
+package br.com.locadora_veiculos.model.veiculo.enums;
+
+public enum TipoVeiculo {
+    AUTOMOVEL("Automóvel"),
+    MOTOCICLETA("Motocicleta"),
+    VAN("Van");
+
+    private String nomeExibicao;
+
+    TipoVeiculo(String nomeExibicao) {
+        this.nomeExibicao = nomeExibicao;
+    }
+
+    public String getNomeExibicao() {
+        return nomeExibicao;
+    }
+
+    @Override
+    public String toString() {
+        return nomeExibicao;
+    }
+
+    public TipoVeiculo fromString(String nome) {
+        for(TipoVeiculo veiculo : TipoVeiculo.values()) {
+            if(veiculo.name().equalsIgnoreCase(nome)) return veiculo;
+        }
+
+        throw new IllegalArgumentException(String.format("%s O tipo de veiculo %s não existe!", nome));
+    }
+}
