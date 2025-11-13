6cbeca2bb71b2a504dddbbd6a2e506dd361afbfd
Victor Hugo <victor.030farias@gmail.com>
2025-11-13 13:41:53 -0300

    Cria interface Dao para veículo

---
+++ src/main/java/br/com/locadora_veiculos/model/veiculo/dao/VeiculoDao.java
@@ -0,0 +1,8 @@
+package br.com.locadora_veiculos.model.veiculo.dao;
+
+import br.com.locadora_veiculos.model.dao.Dao;
+import br.com.locadora_veiculos.model.veiculo.Veiculo;
+
+public interface VeiculoDao extends Dao<Veiculo> {
+    //TODO: VALIDAR IMPLEMENTAÇÃO DE DELETE FROM LIST
+}
